262863bb32a0b86ae74b0168db64a690b7fe34fa
wonjonghong <wonhong1996@naver.com>
2025-05-02 19:15:19 +0900

    feat/로그인 연동중

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart' as kakao;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'ui/app.dart';
 
 void main() async {
   // Flutter 바인딩 초기화
   WidgetsFlutterBinding.ensureInitialized();
 
+  // .env 파일 로드
+  await dotenv.load(fileName: ".env");
+
   // Firebase 초기화
   await Firebase.initializeApp();
 
-  // 카카오 SDK 초기화 (네이티브 앱 키는 카카오 개발자 콘솔에서 발급받아야 함)
-  kakao.KakaoSdk.init(nativeAppKey: 'YOUR_KAKAO_NATIVE_APP_KEY');
+  // 카카오 SDK 초기화
+  final kakaoNativeAppKey = dotenv.env['KAKAO_NATIVE_APP_KEY'] ?? '';
+  kakao.KakaoSdk.init(nativeAppKey: kakaoNativeAppKey);
 
   runApp(const ProviderScope(child: MyApp()));
 }
